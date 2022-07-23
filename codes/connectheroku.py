240a6f4053254912177a111ba07d222f70d242bf
Ali Lara <alilarag@gmail.com>
2022-07-23 15:41:05 -0400

    wip putting EDA all together

--- codes/connectheroku.py
+++ codes/connectheroku.py
@@ -1,4 +1,6 @@
 import json
+
+import pandas as pd
 import psycopg2
 
 
@@ -13,3 +15,22 @@ def connect_database(project, filepath):
         port=credentials['databases']["heroku"][project]['port']
     )
     return conn.cursor()
+
+
+def make_query(query_string, project, filepath):
+    f = open(filepath, "r")
+    credentials = json.load(f)
+    conn = psycopg2.connect(
+        host=credentials['databases']["heroku"][project]['host'],
+        user=credentials['databases']["heroku"][project]['user'],
+        password=credentials['databases']["heroku"][project]['password'],
+        database=credentials['databases']["heroku"][project]['database'],
+        port=credentials['databases']["heroku"][project]['port']
+    )
+    cursorobject = conn.cursor()
+    cursorobject.execute(query_string)
+    results = cursorobject.fetchall()
+    column_names = [i[0] for i in cursorobject.description]
+    cursorobject.close()
+    df = pd.DataFrame(results, columns=column_names)
+    return df
